fe52619b6213890968237c2202f4f137fe28e8c9
shivmundhe7 <shivmundhe16@gmail.com>
2025-08-16 03:49:47 +0530

    JS

---
+++ consolelog.js
@@ -0,0 +1,5 @@
+console.log(10);
+console.log("Hello World")
+
+let age = 13;
+console.log(age,10)
---
+++ datatypes.js
@@ -0,0 +1,40 @@
+// 1) Number 
+// 2) String
+// 3) Boolean
+// 4) Undefined
+// 5) Obejct
+
+// Primitive & Non Primitive
+
+// Primitive = Atomic In Nature
+// Example :- Numbers
+
+// Non Primitive
+// Which are composition of other types
+// example :- Object
+
+// user {
+    //  name = "shiv"
+    //  age = 18
+    //  Posts: {
+                // Createdat = "25Jun 2025"
+                // text: "my firsst post "
+    // }
+    // Gender: "Male"
+// }
+
+
+// Other DataTypes
+var marks = 100;
+var name = "sanket";
+var age;
+var company = null;
+var salary = undefined;
+var isStudent = false;
+
+console.log("Marks = ", marks);
+console.log("Name = ", name);
+console.log("Age = ", age);
+console.log("Company = ", company);
+console.log("salary = ", salary);
+console.log("isStudent= ", isStudent);
