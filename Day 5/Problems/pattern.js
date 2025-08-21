1cb36289a0eb5150ad24019b2deb3c892e73704c
shivmundhe7 <shivmundhe16@gmail.com>
2025-08-22 00:04:06 +0530

    JS git push

---
+++ Day 5/Problems/pattern.js
@@ -0,0 +1,12 @@
+let n = 4;
+for(let row =1; row<=n; row +=1){
+    let str = "";
+    for(let col = 1; col <= n; col +=1){
+        str += "*";
+
+    }
+    console.log(str);
+}
+
+pattern(4);
+pattern(5);
